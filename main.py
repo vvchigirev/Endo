093c2bcced7d44f799ffe57381f5e059b4352130
Victor <46968866+vvchigirev@users.noreply.github.com>
2022-11-14 23:01:43 +0700

    Revert "Главное окно приложения"

--- main.py
+++ main.py
@@ -1,27 +1,16 @@
-# # This is a sample Python script.
-#
-# # Press Shift+F10 to execute it or replace it with your code.
-# # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-#
-#
-# def print_hi(name):
-#     # Use a breakpoint in the code line below to debug your script.
-#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-#
-#
-# # Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
-#     print_hi('PyCharm')
-#
-# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# This is a sample Python script.
 
-import sys
-from src.views.main_window._main_window import MainWindow
-from PyQt5.QtWidgets import QApplication
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
+    print_hi('PyCharm')
 
-    main_window = MainWindow()
-    main_window.show()
-    sys.exit(app.exec_())
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- readme.txt
+++
@@ -1,4 +0,0 @@
-1. Инициализация Qt:
-    pip3 install PyQt5
-    pip install pyqt5
-    pip install PyQt6
