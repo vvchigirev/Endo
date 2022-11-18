f79dac38656251d91fd32aa59fb76630e07ff780
Seryozha <seryozha.ch_130209@ngs.ru>
2022-11-18 20:55:36 +0700

    создал базовый клас для моделей справочников

---
+++ src/common/base_classes/models/base_dict_model.py
@@ -0,0 +1,35 @@
+from typing import Any
+from src.common.base_classes.models.base_model import BaseModel
+
+
+class BaseDictModel(BaseModel):
+    """ Модель. Орган """
+
+    __name = None  # Наименование
+
+    def __init__(self, code: Any = None, name: str = ""):
+        """ Конструктор
+        :param code: Идентификатор
+        :param name: Наименование
+        """
+
+        super().__init__(code)
+
+        self.__name = name
+
+    @property
+    def name(self):
+        """ Свойство. Наименование """
+
+        return self.__name
+
+    @property.setter
+    def name(self, value):
+        """  Свойство (установка).Наименование
+        :param value: значение
+        """
+
+        self.__name = value
+
+    def __str__(self):
+        return f"({self.id}) {self.__name}"
