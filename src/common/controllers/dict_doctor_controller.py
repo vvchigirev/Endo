a688a7d76acc55e2a66a54c8d63fc08f30e7798c
Victor <46968866+vvchigirev@users.noreply.github.com>
2022-11-29 00:41:30 +0700

    В контроллере врачей реализовал метод сохранения данных по Врачу

--- src/common/controllers/dict_doctor_controller.py
+++ src/common/controllers/dict_doctor_controller.py
@@ -1,25 +1,36 @@
 from ...dict.doctors.models.doctor_model import DoctorModel
+from ..data.xml_data_provider import XmlDataProvider
+from ..data.xml_doctors import XmlDoctors
 
 
 class ControllerDictDoctor:
     """ Контроллерю Справочник докторов """
 
-    def select(self):
+    __data_provider: XmlDataProvider = None  # Провайдер данных XML
+    __xml_doctors: XmlDoctors = None  # Xml структур для Врачей
+
+    def __init__(self, xml_provider: XmlDataProvider=None):
+        """ Конструктор
+        :param xml_provider: Xml провайдер
+        """
+
+        self.__data_provider = xml_provider
+        self.__xml_doctors = XmlDoctors()
+
+    def select_all(self):
         """ Получение списка вречей
         :return: Список моделей врачей
         """
 
         return None
 
-    def get(self, kode):
+    def get_el(self, kode):
         """ Получение врача по коду
         :param kode: Код врача
         :return: Модель. Врач
         """
 
         try:
-            doctor = DoctorModel(1, "Иванов", "Иван", "Иванович")
-
             print(f"Добавление врача: f{doctor}")
             return True
         except Exception as e:
@@ -27,24 +38,33 @@ class ControllerDictDoctor:
             print(message, e)
             raise Exception(message)
 
-
         return doc
 
-    def create(self, doctor: DoctorModel):
+    def add_el(self, doctor: DoctorModel):
         """ Добавление сущности Врач
         :param doctor: Модель - Врач
         :return: Результат выполнения
         """
 
+        print(": ControllerDictDoctor.add_el()")
+
         try:
             print(f"Добавление врача: f{doctor}")
+
+            xml_element = self.__data_provider.create_element(
+                self.__xml_doctors.group_name,
+                self.__xml_doctors.element_name)
+
+            if xml_element is not None:
+                self.__xml_doctors.create(xml_element, doctor)
+
             return True
         except Exception as e:
             message = "Ошибка добавления врача"
-            print(message, e)
+            print(message + ": ", e)
             raise Exception(message)
 
-    def update(self, doctor: DoctorModel):
+    def update_el(self, doctor: DoctorModel):
         """ Обновление сущности Врач
         :param doctor: Модель - Врач
         :return: Результат выполнения
@@ -58,7 +78,7 @@ class ControllerDictDoctor:
             print(message, e)
             raise Exception(message)
 
-    def delete(self, doctor: DoctorModel):
+    def delete_el(self, doctor: DoctorModel):
         """ Удаление сущности Врач
         :param doctor: Модель - Врач
         :return: результат выполнения
@@ -71,6 +91,3 @@ class ControllerDictDoctor:
             message = "Ошибка удаления врача"
             print(message, e)
             raise Exception(message)
-
-
-
