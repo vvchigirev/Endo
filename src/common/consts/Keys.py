033d5ec25452f1a69c326677a161592099c5af3f
Victor <46968866+vvchigirev@users.noreply.github.com>
2022-11-29 00:13:03 +0700

    Добавил файл кючей

---
+++ src/common/consts/Keys.py
@@ -0,0 +1,11 @@
+class Keys:
+    """ ключи доступа к словарным значениям """
+
+    DOCTORS = 'Doctors'
+    DOCTOR = "Doctor"
+    NURSES = "Nurses"
+    NURSE = "Nurse"
+    DEVICES = "Devices"
+    DEVICE = "Device"
+    ORGANS = "Organs"
+    ORGAN = "Organ"
