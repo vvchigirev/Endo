ff789e3f9c642dabc93ff6a1c8a33b1335d11074
Victor <46968866+vvchigirev@users.noreply.github.com>
2022-12-01 22:49:12 +0700

    Добавил базовый вспомогательные классы для описания наименваний секций в Xml документе

--- src/common/base_classes/base_sections.py
+++ src/common/base_classes/base_sections.py
@@ -1,17 +1,17 @@
 import xml.etree.ElementTree as ET
 
 
-class BaseXmlElement:
-    """ Базовый класс для xml структур"""
+class BaseSections:
+    """ Базовый класс для наименования xml секций """
 
-    __group_name = None
-    __element_name = None
+    _group_name: str = ""
+    _element_name: str = ""
 
-    def __init__(self):
+    def __init__(self, el_group_name=None, el_name=None):
         """ Конструктор """
 
-        self.__group_name = None
-        self.__element_name = None
+        self._group_name = el_group_name
+        self._element_name = el_name
 
     # region Свойства
 
@@ -19,7 +19,7 @@ class BaseXmlElement:
     def group_name(self):
         """ Свойство. Имя группы """
 
-        return self.__group_name
+        return self._group_name
 
     @group_name.setter
     def group_name(self, value):
@@ -27,17 +27,26 @@ class BaseXmlElement:
         :param value: Устанавливаемое значение
         """
 
-        self.__group_name = value
+        self._group_name = value
 
     @property
     def element_name(self):
         """ Свойство. Имя элемента """
 
-        return self.__element_name
+        return self._element_name
 
     @element_name.setter
     def element_name(self, value):
         """ Свойство (установка). Имя элемента """
-        self.__element_name = value
+
+        self._element_name = value
 
     # endregion
+
+    def __str__(self):
+        s = ""
+        s += "group_name= " + self.group_name + ", "
+        s += "element_name= " + self.element_name
+
+
+        return s
