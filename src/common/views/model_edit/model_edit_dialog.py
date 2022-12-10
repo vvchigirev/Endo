8b6b95b312882219f615aecd3610b76f5adf0128
Victor <46968866+vvchigirev@users.noreply.github.com>
2022-12-10 11:14:43 +0700

    ENDO-9. Создано диалоговое окно для отображения виджетов редактирования сущностей

--- src/common/views/model_edit/model_edit_dialog.py
+++ src/common/views/model_edit/model_edit_dialog.py
@@ -1,7 +1,6 @@
 import os
 from PyQt5 import uic, QtWidgets
-from ...dict.doctors.views.doctor_edit import DoctorEditWidget
-
+from src.common.base_classes.views.base_model_edit_widget import BaseModelEditWidget
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(os.path.dirname(__file__), 'model_edit_dialog.ui'))
 
@@ -9,9 +8,9 @@ FORM_CLASS, _ = uic.loadUiType(os.path.join(os.path.dirname(__file__), 'model_ed
 class ModelEditDialog(QtWidgets.QDialog, FORM_CLASS):
     """Диалог редактирования модели"""
 
-    __show_widget = None    # Показываемый виджет
+    __show_widget: BaseModelEditWidget = None    # Показываемый виджет
 
-    def __init__(self, widget):
+    def __init__(self, widget: BaseModelEditWidget):
         """Конструктор"""
 
         print(": ModelEditDialog.__init__()")
@@ -23,23 +22,41 @@ class ModelEditDialog(QtWidgets.QDialog, FORM_CLASS):
 
         self.__prepare_ui()
 
+    def get_model(self):
+        """ Получение подели из вложенного виджета """
+
+        print(": BaseModelEditWidget.get_model()")
+
+        model = self.__show_widget.get_model()
+
+        print("model=", model)
+        return model
+
     def __prepare_ui(self):
         """ Подготовка окна """
 
         print(": ModelEditDialog.__prepare_ui()")
-        self.setModal(True)
 
-        print("1")
+        self.setModal(True)
+        self.setWindowTitle("Привет")
+
         self.buttonBox.accepted.connect(self.__on_click_btn_accept)
-        print("2")
         self.buttonBox.rejected.connect(self.close)
 
-        print("3")
         print("self.__show_widget=", self.__show_widget)
         self.layoutContaner.addWidget(self.__show_widget)
-        print("4")
 
     def __on_click_btn_accept(self):
         """ Обработчик события нажатия на кнопку 'OK' """
 
         print("ОК")
+
+    def accept(self):
+        """ Событие положительного закрытия формы """
+
+        print(": ModelEditDialog.accept()")
+
+        if self.__show_widget.check_input_values():
+            super(ModelEditDialog, self).accept()
+        else:
+            return
