9b2d779938c9d498db35cfecbca15a4decd41621
Victor <46968866+vvchigirev@users.noreply.github.com>
2022-12-18 02:42:56 +0700

    Подчищена главная форма

--- src/views/main_window/_main_window.py
+++ src/views/main_window/_main_window.py
@@ -27,11 +27,8 @@ FORM_CLASS, _ = uic.loadUiType(os.path.join(os.path.dirname(__file__), '_main_wi
 class MainWindow(QMainWindow, FORM_CLASS):
     """ Главное окно """
 
-    __xml_provider = None  # Провайдер xml документа
-    __controller_doctors: ControllerDictDoctor = None  # Контроллер справочника врачей
-    __controller_organs: ControllerDictOrgan = None
-    __controller_devices: ControllerDictDevice = None
-    __current_widget_dict:BaseDictModelListWidget = None        # Текущий виджет показывающий справочник
+    __xml_provider = None                               # Провайдер xml документа
+    __current_widget_dict: BaseDictModelListWidget = None        # Текущий виджет показывающий справочник
 
     def __init__(self):
         """ Конструктор """
@@ -47,16 +44,7 @@ class MainWindow(QMainWindow, FORM_CLASS):
         print("XmlDataProvider.root=", XmlDataProvider.root)
         print("self.__xml_provider.root=", self.__xml_provider.root)
 
-        # ***
-        # self.__controller_doctors = ControllerDictDoctor(self.__xml_provider)
-        self.__controller_doctors = ControllerDictDoctor()
-        self.__controller_organs = ControllerDictOrgan(self.__xml_provider)
-        self.__controller_devices = ControllerDictDevice(self.__xml_provider)
-
         self.__current_widget_dict = None
-
-        # list_organs_widget = OrgansListWidget(self.__xml_provider, parent=self)
-        # self.layoutContaner_2.addWidget(list_organs_widget)
 
     def __prepare_ui(self):
         """ Подготовка интерфейса """
@@ -79,7 +67,6 @@ class MainWindow(QMainWindow, FORM_CLASS):
         self.pushButtonDictDelete.clicked.connect(self.__on_clicked_pushButtonDictDelete)
 
         self.pushBtn.clicked.connect(self.__on_pushBtn_clicked)
-        # self.toolBtn.clicked.connect(self.__on_toolBtn_clicked)
 
     def __create_menu(self):
         """ Создание меню """
@@ -93,17 +80,17 @@ class MainWindow(QMainWindow, FORM_CLASS):
         menu_file = menubar.addMenu('Файл')
         menu_file.addAction(exit_action)
 
-        load_action = QAction('Загрузить', self)
-        load_action.setStatusTip("Загрузить данные")
-        load_action.triggered.connect(self.__on_click_menu_item_data_load)
+        # load_action = QAction('Загрузить', self)
+        # load_action.setStatusTip("Загрузить данные")
+        # load_action.triggered.connect(self.__on_click_menu_item_data_load)
 
-        save_action = QAction('Сохранить', self)
-        save_action.setStatusTip("Сохранить данные")
-        save_action.triggered.connect(self.__on_click_menu_item_data_save)
+        # save_action = QAction('Сохранить', self)
+        # save_action.setStatusTip("Сохранить данные")
+        # save_action.triggered.connect(self.__on_click_menu_item_data_save)
 
-        menu_operation = menubar.addMenu("Операции")
-        menu_operation.addAction(load_action)
-        menu_operation.addAction(save_action)
+        # menu_operation = menubar.addMenu("Операции")
+        # menu_operation.addAction(load_action)
+        # menu_operation.addAction(save_action)
 
         # menu_dict = menubar.addMenu('Справочники')
 
@@ -115,20 +102,6 @@ class MainWindow(QMainWindow, FORM_CLASS):
 
         # self.__xmlData.write()
 
-    def __on_click_menu_item_data_load(self):
-        print("Загрузить данные")
-
-        doctor = DoctorModel(1, "Иванов", "Иван", "Иванови")
-        self.__controller_doctors.create_doctor(doctor)
-
-        # doctor = DoctorModel(5, "Сидоров", "Сидр", "Сидорови")
-        # self.__controller_doctors.create_doctor(doctor)
-
-    def __on_click_menu_item_data_save(self):
-        print("Сохранить данные")
-
-        # self.xmlData.write()
-
     def __on_triggered_menuItemDictDoctors(self):
         """ Обработчик нажатия на кнопку 'Справочник' """
 
@@ -137,7 +110,7 @@ class MainWindow(QMainWindow, FORM_CLASS):
         if self.__current_widget_dict:
             self.__current_widget_dict.hide()
 
-        list_doctors_widget = DoctorsListWidget(self.__xml_provider, parent=self)
+        list_doctors_widget = DoctorsListWidget(parent=self)
         self.__current_widget_dict = list_doctors_widget
 
         self.layoutContaner.addWidget(list_doctors_widget)
@@ -150,13 +123,7 @@ class MainWindow(QMainWindow, FORM_CLASS):
         if self.__current_widget_dict:
             self.__current_widget_dict.hide()
 
-        # list_organs = self.__controller_organs.select_organs()
-        # for organ in list_organs:
-        #     print(f'- {organ}')
-
         list_organs_widget = OrgansListWidget(self.__xml_provider, parent=self)
-        print('list_organs_widget:', list_organs_widget)
-
         self.__current_widget_dict = list_organs_widget
 
         # self.layoutContaner_2.addWidget(list_organs_widget)
@@ -170,34 +137,25 @@ class MainWindow(QMainWindow, FORM_CLASS):
         if self.__current_widget_dict:
             self.__current_widget_dict.hide()
 
-        list_endos_widget = EndosListWidget()
+        list_endos_widget = EndosListWidget(parent=self)
 
         self.__current_widget_dict = list_endos_widget
 
         # self.layoutContaner_2.addWidget(list_endos_widget)
         self.layoutContaner.addWidget(list_endos_widget)
 
-    # def __on_pushBtnDict_device_clicked(self):
-    #     """ Обработчик нажатия на кнопку 'Справочник' """
-    #
-    #     list_devices = self.__controller_devices.select_devices()
-    #     for device in list_devices:
-    #         print(f'- {device}')
-
     def __on_triggered_menuItemDictDevices(self):
-        """ Обработчик выбора пункта меню 'Справочник 'Приборов'' """
+        """ Обработчик выбора пункта меню 'Справочник Приборов' """
 
         print(": MainWindow.__on_triggered_menuItemDictDevices")
 
         if self.__current_widget_dict:
             self.__current_widget_dict.hide()
 
-        list_devices_widget = DevicesListWidget()
-
+        list_devices_widget = DevicesListWidget(parent=self)
         self.__current_widget_dict = list_devices_widget
 
         self.layoutContaner.addWidget(list_devices_widget)
-
 
     def __on_pushBtn_clicked(self):
         print(": MainWindow.__on_puchBtn_clicked()")
@@ -206,7 +164,7 @@ class MainWindow(QMainWindow, FORM_CLASS):
     def __on_clicked_pushButtonDictRefresh(self):
         """ Обработчик события нажатия на кнопку 'Обновить' """
 
-        print(": MainWindow.pushButtonDictRefresh()")
+        # print(": MainWindow.pushButtonDictRefresh()")
 
         if self.__current_widget_dict:
             self.__current_widget_dict.refresh()
@@ -214,24 +172,7 @@ class MainWindow(QMainWindow, FORM_CLASS):
     def __on_clicked_pushButtonDictCreate(self):
         """ Обработчик события нажатия на кнопку 'Добавить' """
 
-        print(": MainWindow.__on_clicked_pushButtonDictCreate()")
-
-        # # QMessageBox.information(self, "Инфо", "Добавим модель", QMessageBox.Ok)
-        #
-        # doctor = DoctorModel(55, "Иванов", "Иван", "Иванович")
-        # edit_doctor_widget = DoctorEditWidget(doctor, self)
-        # print("edit_doctor_widget=", edit_doctor_widget)
-        #
-        # edit_dlg = ModelEditDialog(edit_doctor_widget)
-        # edit_dlg.show()
-        # result = edit_dlg.exec_()
-        # if result:
-        #     model_edit = edit_dlg.get_model()
-        #     print("model_edit=", model_edit)
-        #
-        #     print("Форма закрылась положительно")
-        # else:
-        #     print("Форма закрылась отрицательно!")
+        # print(": MainWindow.__on_clicked_pushButtonDictCreate()")
 
         if self.__current_widget_dict:
             self.__current_widget_dict.create_element()
@@ -239,7 +180,7 @@ class MainWindow(QMainWindow, FORM_CLASS):
     def __on_clicked_pushButtonDictUpdate(self):
         """ Обработчик события нажатия на кнопку 'Изменить' """
 
-        print(": MainWindow.__on_clicked_pushButtonDictUpdate()")
+        # print(": MainWindow.__on_clicked_pushButtonDictUpdate()")
 
         if self.__current_widget_dict:
             self.__current_widget_dict.update_element()
@@ -247,7 +188,7 @@ class MainWindow(QMainWindow, FORM_CLASS):
     def __on_clicked_pushButtonDictDelete(self):
         """ Обработчик события нажатия на кнопку 'Удалить' """
 
-        print(": MainWindow.__on_clicked_pushButtonDictDelete()")
+        # print(": MainWindow.__on_clicked_pushButtonDictDelete()")
 
         if self.__current_widget_dict:
             self.__current_widget_dict.delete_element()
