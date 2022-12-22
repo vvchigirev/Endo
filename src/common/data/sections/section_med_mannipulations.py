d72cfa16625a4ff5f8042961ca519ab450493a57
Seryozha <seryozha.ch_130209@ngs.ru>
2022-12-22 15:18:39 +0700

    ENDO-13. Создал секцию для справочника лечебных манипуляций

---
+++ src/common/data/sections/section_med_mannipulations.py
@@ -0,0 +1,11 @@
+from ...base_classes.base_sections import BaseSections
+from ...consts.Keys import Keys
+
+
+class SectionMedManipulations(BaseSections):
+    """ Наименовая блоков элементов для справочника Приборов """
+
+    def __init__(self):
+        """ Конструктор """
+
+        super(SectionMedManipulations, self).__init__(Keys.MED_MANIPULATIONS, Keys.MED_MANIPULATION)
